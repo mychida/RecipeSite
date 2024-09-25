9d09f8ee712b5d30e6f9d297bb03bd7091067d73
uchida <uchidada14916@gmail.com>
2024-09-25 22:07:34 +0900

    first commit

---
+++ src/test/java/com/example/RecipeSite/RecipeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
